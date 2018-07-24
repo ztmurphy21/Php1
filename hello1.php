7367c4303e1378ff164bc2be499d5c83f047aa20
Zachary Murphy <ztmurphy21@gmail.com>
2018-07-23 21:49:46 -0500

    more php practice
    outputing text to html and comments within code.

---
+++ hello1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="ut-8">
+    <style type="text/css">
+        .bold{
+            font-weight: bolder;
+        }
+    </style>
+    
+    <title>Hello, World!</title>
+</head>
+<body>
+<p>The following was created by PHP:
+<?php
+    print "<span class=\"bold\">Hello, world!</span>";
+?>
+    
+</p>    
+</body>
+</html>
---
+++ hello3.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="ut-8">
+    <style type="text/css">
+        .bold{
+            font-weight: bolder;
+        }
+    </style>
+    
+    <title>Hello, World!</title>
+</head>
+<body>
+<p>The following was created by PHP:
+<?php
+    
+    /*
+    *Filename: hello3.php
+    *Created by: Zach Murphy
+    */
+    
+    //greeting
+    print "<span class=\"bold\"> Hello, world! </span>";
+?>
+    
+</p>
+    <!-- This is a HTML commet-->
+</body>
+</html>
